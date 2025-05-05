bfef96f575d5052ff5cb1dec3701fd1b23f876e7
temycodes <temycodes@gmail.com>
2025-05-05 12:34:13 +0100

    chore: code cleanup

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function moveElements() {
   const pitchWidth = pitch.clientWidth;
   const pitchHeight = pitch.clientHeight;
 
-  const player1X = Math.random() * (pitchWidth - 20); // subtract element width
+  const player1X = Math.random() * (pitchWidth - 20);
   const player1Y = Math.random() * (pitchHeight - 20);
   player1.style.transform = `translate(${player1X}px, ${player1Y}px)`;
 
@@ -16,7 +16,7 @@ function moveElements() {
   const player2Y = Math.random() * (pitchHeight - 20);
   player2.style.transform = `translate(${player2X}px, ${player2Y}px)`;
 
-  const ballX = Math.random() * (pitchWidth - 15); // smaller size
+  const ballX = Math.random() * (pitchWidth - 15); 
   const ballY = Math.random() * (pitchHeight - 15);
   ball.style.transform = `translate(${ballX}px, ${ballY}px)`;
 }
